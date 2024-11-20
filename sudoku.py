eec28cbe93c37deaa58d94fe1c7f8ad8dfbaae09
GabrielVecchione <gvecchione08@gmail.com>
2024-11-20 13:27:22 -0500

    Revert "Update sudoku.py"
    This reverts commit 4a4d9897b380ad9d45fc88477d5be66b88f157cf.

--- sudoku.py
+++ sudoku.py
@@ -1,1 +1,31 @@
-print("Yo was good guyz")
+import numpy as np
+import random
+
+#CLASSES
+class Board:
+
+    class Square:
+        def __init__(self, row:int, col:int, value:int, locked:bool):
+            self.row = row
+            self.col = col
+            self.value = value
+            self.locked = locked
+
+        def __str__(self):
+            return self.value
+
+    def __init__(self):
+        self.grid = [[self.Square(row, col, random.randint(0,9), False) for col in range(9)] for row in range(9)]
+
+    def __str__(self):
+        return '\n'.join([' '.join([str(square.value) for square in row]) for row in self.grid])
+
+    def solve(self):
+        ...
+
+#OBJECT CREATION
+bo = Board()
+print(bo)
+
+bo.solve()
+
